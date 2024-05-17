2f16741b543d8352f22902a3e844645c34189bd3
JesseNwaokolo <109309010+JesseNwaokolo@users.noreply.github.com>
2024-05-17 16:56:22 -0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -305,7 +305,7 @@ const animationTimeline = () => {
 };
 
 // Run fetch and animation in sequence
-window.addEventListener("DOMContentLoaded", event => {
+window.addEventListener("load", event => {
   const audio = document.querySelector("#audio");
   audio.volume = 0.2;
   audio.play();
